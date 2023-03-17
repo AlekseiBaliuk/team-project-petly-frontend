8e546afd36f4afa60f4d4dd1648884812a7174fa
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-03-17 14:30:57 +0200

    Update Logo.styled.jsx

--- src/components/Logo/Logo.styled.jsx
+++ src/components/Logo/Logo.styled.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import {theme} from '../../constants/theme'
+import { theme } from '../../constants/theme';
 
 export const LogoLink = styled(Link)`
   color: ${theme.colors.mainText};
