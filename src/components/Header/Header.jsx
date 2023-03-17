5dcb2ab7a56ec1eb997da7179c0fcfce02cbe3f5
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-03-17 14:45:43 +0200

    Create Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import { Container } from "components/Container/Container.styled"
+import { Logo } from "components/Logo/Logo"
+
+export const Header = () => {
+    return(
+        <header>
+            <Container>
+                <Logo />
+            </Container>
+        </header>
+    )
+}
