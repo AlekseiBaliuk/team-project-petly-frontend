7104b4abb5dcd437e4b0530605ac0ec22491c8d6
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-03-18 11:39:22 +0200

    adaptive icon

--- src/components/UserNav/UserNav.jsx
+++ src/components/UserNav/UserNav.jsx
@@ -2,10 +2,17 @@ import * as SC from './UserNav.styled';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export const UserNav = () => {
+  const iconSize = {
+    fontSize: 20,
+    '@media (min-width: 780px)': {
+      fontSize: 28,
+    },
+  };
+
   return (
     <SC.Nav>
       <SC.Link to="/user">
-        <AccountCircleIcon sx={{ fontSize: 20, marginRight: '12px' }} />
+        <AccountCircleIcon sx={{ ...iconSize, marginRight: '12px' }} />
         <p>Account</p>
       </SC.Link>
     </SC.Nav>
