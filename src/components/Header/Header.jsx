3c46022bcbcd174169e7ff4679c09856be372ed2
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-03-18 16:53:50 +0200

    burger tablet

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { AuthNav } from 'components/AuthNav/AuthNav';
 import { Container } from 'components/Container/Container.styled';
 import { Logo } from 'components/Logo/Logo';
@@ -11,35 +11,92 @@ import CloseIcon from '@mui/icons-material/Close';
 
 export const Header = () => {
   const [menu, setMenu] = useState(false);
-  const toggleMenu = () => setMenu(value => !value);
+  const [width, setWindowWidth] = useState(0);
+  useEffect(() => {
+    updateDimensions();
 
-  return (
-    <header style={{ marginBottom: '44px' }}>
-      <Container>
-        <Toolbar
-          sx={{
-            justifyContent: 'space-between',
-            paddingTop: '16px',
-            marginBottom: '44px',
-          }}
-        >
-          <Logo />
-          <IconButton color="inherit" onClick={toggleMenu}>
-            {!menu ? (
-              <MenuIcon sx={{ fontSize: '40px' }} />
-            ) : (
-              <CloseIcon sx={{ fontSize: '40px' }} />
-            )}
-          </IconButton>
-        </Toolbar>
-        {menu && (
-          <Box height={'100vh'}>
+    window.addEventListener('resize', updateDimensions);
+    return () => window.removeEventListener('resize', updateDimensions);
+  }, []);
+  const updateDimensions = () => {
+    const width = window.innerWidth;
+    setWindowWidth(width);
+  };
+  const toggleMenu = () => setMenu(value => !value);
+  if (width < 768) {
+    return (
+      <header style={{ marginBottom: '44px' }}>
+        <Container>
+          <Toolbar
+            sx={{
+              justifyContent: 'space-between',
+              paddingTop: '16px',
+              marginBottom: '44px',
+            }}
+          >
+            <Logo />
+            <IconButton color="inherit" onClick={toggleMenu}>
+              {!menu ? (
+                <MenuIcon sx={{ fontSize: '40px' }} />
+              ) : (
+                <CloseIcon sx={{ fontSize: '40px' }} />
+              )}
+            </IconButton>
+          </Toolbar>
+          {menu && (
+            <Box height={'100vh'}>
+              <AuthNav />
+              <UserNav />
+              <Nav />
+            </Box>
+          )}
+        </Container>
+      </header>
+    );
+  }
+
+  if (width >= 768 && width < 1200) {
+    return (
+      <header style={{ marginBottom: '44px' }}>
+        <Container>
+          <Toolbar
+            sx={{
+              justifyContent: 'space-between',
+              paddingTop: '16px',
+              marginBottom: '44px',
+            }}
+          >
+            <Logo />
             <AuthNav />
             <UserNav />
-            <Nav />
-          </Box>
-        )}
-      </Container>
-    </header>
-  );
+            <IconButton color="inherit" onClick={toggleMenu}>
+              {!menu ? (
+                <MenuIcon sx={{ fontSize: '40px' }} />
+              ) : (
+                <CloseIcon sx={{ fontSize: '40px' }} />
+              )}
+            </IconButton>
+          </Toolbar>
+          {menu && (
+            <Box height={'100vh'}>
+              <Nav />
+            </Box>
+          )}
+        </Container>
+      </header>
+    );
+  }
+
+  if (width > 1200) {
+    return (
+      <header style={{ marginBottom: '44px' }}>
+        <Container>
+          <Logo />
+          <Nav />
+          <AuthNav />
+          <UserNav />
+        </Container>
+      </header>
+    );
+  }
 };
