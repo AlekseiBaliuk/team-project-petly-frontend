a2043a9ef3dde0ef1fbe3c0cadb9edc993c55594
Yurkes2020 <yurkes2020@gmail.com>
2023-03-19 16:21:45 +0200

    Suspence

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Header } from 'components/Header/Header';
 import { Outlet } from 'react-router-dom';
+import { Loader } from 'components/Loader/Loader';
 
 const SharedLayout = () => {
   return (
     <div>
       <Header />
-      <Outlet />
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
     </div>
   );
 };
