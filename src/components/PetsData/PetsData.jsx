6bf38921005062c6dd168f4129b22153c0b06eff
serhiiarkhypiuk <airborn.gnus@gmail.com>
2023-03-19 11:54:53 +0200

    Added User page template

---
+++ src/components/PetsData/PetsData.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+// import ModalAddPet from 'components/ModalAddPet/ModalAddPet';
+import { Loader } from 'components/Loader/Loader';
+import PetsList from 'components/PetsList/PetsList';
+import {
+  PetsWrapper,
+  PetsTitleWrapper,
+  UserPetsTitle,
+  PetBtnWrapper,
+  AddPetTitleBtn,
+  AddPetBtn,
+  NonPetWrapper,
+} from './PetsData.styled';
+import { PetList } from 'components/PetsList/PetsList.styled';
+
+export default function PetsData({ pets, setPets, petsData }) {
+  // const isPets = Boolean(petsData.length);
+  const isPets = true;
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <PetsWrapper>
+      <PetsTitleWrapper>
+        <UserPetsTitle>Pets</UserPetsTitle>
+        <PetBtnWrapper>
+          <AddPetTitleBtn>Add</AddPetTitleBtn>
+          <AddPetBtn onClick={() => setIsOpen(true)} />
+        </PetBtnWrapper>
+      </PetsTitleWrapper>
+      {/*{isOpen ? (*/}
+      {/*  <ModalAddsPetApp*/}
+      {/*    petsData={pets}*/}
+      {/*    setPets={setPets}*/}
+      {/*    onOpenModal={}*/}
+      {/*    onCloseModal={}*/}
+      {/*  />*/}
+      {/*) : null}*/}
+      {isPets ? (
+        <PetList />
+      ) : (
+        <NonPetWrapper>
+          <p>No pets</p>
+        </NonPetWrapper>
+      )}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <PetsList dataPets={pets} setDataPets={setPets} />
+      )}
+    </PetsWrapper>
+  );
+}
