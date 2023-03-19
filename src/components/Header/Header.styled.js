4a55becf3b45670db83b62d162c83138b9ab9279
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-03-19 10:56:44 +0200

    header padding

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -13,7 +13,14 @@ export const Header = styled.header`
 export const ToolBar = styled.div`
   display: flex;
   align-items: center;
+  padding-top: 16px;
   @media (max-width: 767px) {
     justify-content: space-between;
+  }
+  @media ${device.tablet} {
+    padding-top: 24px;
+  }
+  @media ${device.desktop} {
+    margin-bottom: 20px;
   }
 `;
