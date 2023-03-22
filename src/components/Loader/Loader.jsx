8954efc18802e3b012674c81e52b35d639d87fce
serhiiarkhypiuk <airborn.gnus@gmail.com>
2023-03-22 19:46:10 +0200

    Optional render

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1317,12 +1317,24 @@ export const Loader = ({ size = 200, color = '#f59256' }) => {
 
   return (
     <>
-      {/*{randomizer > 0 && randomizer <= 200 && <CatLoader size={size} color={color}/>}*/}
-      {/*{randomizer > 200 && randomizer <= 400 && <PigLoader size={size} color={color}/>}*/}
-      {/*{randomizer > 400 && randomizer <= 600 && <KappiLoader size={size} color={color}/>}*/}
-      {/*{randomizer > 600 && randomizer <= 800 && <DogLoader size={size} color={color}/>}*/}
-      {/*{randomizer > 800 && randomizer <= 1000 && <FoxLoader size={size} color={color}/>}*/}
-      {/*{randomizer > 1000 && randomizer <= 1200 && <GhostLoader size={size} color={color}/>}*/}
+      {randomizer > 0 && randomizer <= 200 && (
+        <CatLoader size={size} color={color} />
+      )}
+      {randomizer > 200 && randomizer <= 400 && (
+        <PigLoader size={size} color={color} />
+      )}
+      {randomizer > 400 && randomizer <= 600 && (
+        <KappiLoader size={size} color={color} />
+      )}
+      {randomizer > 600 && randomizer <= 800 && (
+        <DogLoader size={size} color={color} />
+      )}
+      {randomizer > 800 && randomizer <= 1000 && (
+        <FoxLoader size={size} color={color} />
+      )}
+      {randomizer > 1000 && randomizer <= 1200 && (
+        <GhostLoader size={size} color={color} />
+      )}
     </>
   );
 };
