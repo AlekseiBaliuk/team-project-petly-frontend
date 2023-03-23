b7270b29b2c51475f8eed5e15ec1845e98ae929e
serhiiarkhypiuk <airborn.gnus@gmail.com>
2023-03-23 21:22:01 +0200

    Logout redirect to main page added

--- src/components/UserPage/Logout/Logout.jsx
+++ src/components/UserPage/Logout/Logout.jsx
@@ -7,10 +7,12 @@ import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogTitle from '@mui/material/DialogTitle';
+import { useNavigate } from 'react-router-dom';
 
 const Logout = () => {
   const [open, setOpen] = useState(false);
   const dispatch = useDispatch();
+  let navigate = useNavigate();
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -41,6 +43,7 @@ const Logout = () => {
           <Button
             onClick={() => {
               dispatch(logOut());
+              navigate('/');
             }}
             autoFocus
             style={{ color: '#F59256' }}
