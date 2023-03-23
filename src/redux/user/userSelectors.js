f4e66b188a037b5c8e0749664fa63b1eb7a9a0b2
serhiiarkhypiuk <airborn.gnus@gmail.com>
2023-03-23 20:38:42 +0200

    useUser hook used :)

--- src/redux/user/userSelectors.js
+++ src/redux/user/userSelectors.js
@@ -1,10 +1,5 @@
 export const getUserData = ({ user }) => user;
 
-export const getLoading = ({ user }) => user.isLoading;
-export const getError = ({ user }) => user.error;
-
-//
-
 export const selectUserData = state => state.user.user;
 
 export const selectUserPets = state => state.user.userPets;
