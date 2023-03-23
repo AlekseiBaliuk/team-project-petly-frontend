526ee7af9614ef0bedcda513c1063fa5fd6b8db7
Aleksei Baliuk <aleksred.b@gmail.com>
2023-03-23 23:50:30 +0200

    fix refresh

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -58,7 +58,7 @@ export const refreshUser = createAsyncThunk(
 
     try {
       setAuthHeader(persistedToken);
-      const res = await axios.get('/users');
+      const res = await axios.get('/users/current');
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
