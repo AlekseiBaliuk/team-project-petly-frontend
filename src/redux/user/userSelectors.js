c4887909a18a2dcb3c716622ee26971ea2983032
Aleksei Baliuk <aleksred.b@gmail.com>
2023-03-23 12:25:31 +0200

    add user redux

--- src/redux/user/userSelectors.js
+++ src/redux/user/userSelectors.js
@@ -2,3 +2,13 @@ export const getUserData = ({ user }) => user;
 
 export const getLoading = ({ user }) => user.isLoading;
 export const getError = ({ user }) => user.error;
+
+//
+
+export const selectUserData = state => state.user.user;
+
+export const selectUserPets = state => state.user.userPets;
+
+export const selectIsLoading = state => state.user.isLoading;
+
+export const selectError = state => state.user.error;
