0a0a125ab0cee5afb7e58451059ceab42a8db8f1
Yurkes2020 <yurkes2020@gmail.com>
2023-03-26 20:29:17 +0300

    fix

--- src/components/Notice/NoticeCategoryList/NoticeCategoryList.jsx
+++ src/components/Notice/NoticeCategoryList/NoticeCategoryList.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useCategory } from 'hooks/useCategory';
 import selectors from 'redux/notices/noticesSelectors';
@@ -10,13 +10,20 @@ import {
 import NoticeCategoryItem from '../NoticeCategoryItem';
 import style from './NoticeCategoryList.styled';
 
-const { selectNotices, selectIsAdded } = selectors;
+const {
+  selectNotices,
+  selectIsAdded,
+  // selectTotal
+} = selectors;
 
 export const NoticeCategoryList = ({ search }) => {
-  const { Grid, Scroll } = style;
-
   const noticesList = useSelector(selectNotices);
   const isAdded = useSelector(selectIsAdded);
+  // const total = useSelector(selectTotal);
+
+  const [page, setPage] = useState(1);
+  // const [pets, setPets] = useState(noticesList);
+  const { Grid, Scroll } = style;
 
   const { activeCategory } = useCategory();
   const dispatch = useDispatch();
@@ -25,19 +32,19 @@ export const NoticeCategoryList = ({ search }) => {
     const fetch = async () => {
       switch (activeCategory) {
         case 'favorite':
-          await dispatch(getFavorites());
+          await dispatch(getFavorites(page));
           break;
         case 'my-ads':
-          await dispatch(getMyNotices());
+          await dispatch(getMyNotices(page));
           break;
 
         default:
-          await dispatch(fetchNotices(activeCategory));
+          await dispatch(fetchNotices({ activeCategory, page }));
           break;
       }
     };
     fetch();
-  }, [activeCategory, dispatch, isAdded]);
+  }, [activeCategory, dispatch, isAdded, page]);
 
   function filterNotice(list) {
     if (search.length === 0) {
@@ -50,7 +57,19 @@ export const NoticeCategoryList = ({ search }) => {
     return filterList;
   }
 
+  // const handleScroll = () => {
+  //   if (
+  //     window.innerHeight + document.documentElement.scrollTop + 1 >=
+  //     document.documentElement.scrollHeight
+  //   ) {
+  //     setPage(prev => prev + 1);
+  //   }
+  // };
+
+  // useEffect(() => window.addEventListener('scroll', handleScroll));
+
   const scrollTo = () => {
+    setPage(1);
     return window.scrollTo({
       top: 0,
       left: 0,
@@ -62,6 +81,7 @@ export const NoticeCategoryList = ({ search }) => {
     <>
       <Grid>
         {filterNotice(noticesList).length > 0 &&
+          // filterNotice(pets).length <= total &&
           filterNotice(noticesList).map(notice => (
             <NoticeCategoryItem key={notice._id} fetch={notice} />
           ))}
