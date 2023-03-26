971fd379d7d79d30cda8f9a24937a3682772c86c
serhiiarkhypiuk <airborn.gnus@gmail.com>
2023-03-26 19:40:09 +0300

    Added User pets data render

--- src/components/UserPage/PetsData/PetsData.jsx
+++ src/components/UserPage/PetsData/PetsData.jsx
@@ -1,10 +1,6 @@
-import React, { useState } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { getPetInfo } from 'redux/pets/petsOperations';
-// import { getLoading, getError, getPets } from 'redux/pets/petsSelectors';
-// import ModalAddPet from 'components/ModalAddPet/ModalAddPet';
-// import { Loader } from 'components/Loader/Loader';
-// import PetsList from 'components/UserPage/PetsList/PetsList';
+import React, { useEffect, useState } from 'react';
+import { Loader } from 'components/Loader/Loader';
+import PetsList from 'components/UserPage/PetsList/PetsList';
 import {
   PetsWrapper,
   PetsTitleWrapper,
@@ -17,15 +13,19 @@ import {
 import { PetList } from 'components/UserPage/PetsList/PetsList.styled';
 import { FirstStep } from '../ModalAddsPet/FirstStep';
 import { SecondStep } from '../ModalAddsPet/SecondStep';
+import { useUser } from '../../../hooks/useUser';
 
-const PetsData = ({ pets, setPets }) => {
-  // const isLoading = useSelector(getLoading);
-  // const error = useSelector(getError);
-  // const petsData = useSelector(getPets);
-  // const isPets = Boolean(petsData.length);
-  const isPets = false;
+const PetsData = () => {
+  const { isLoading, error, userPets } = useUser();
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    if (pets !== userPets && userPets !== undefined) {
+      setPets(userPets);
+    }
+  }, [userPets]);
+
   const [isModalShow, setIsModalShow] = useState('none');
-  const [isShown, setIsShown] = useState(false);
   const initialValuesModalData = {
     name: '',
     birthday: '',
@@ -35,13 +35,7 @@ const PetsData = ({ pets, setPets }) => {
   };
   const [modalData, setModalData] = useState(initialValuesModalData);
 
-  // const dispatch = useDispatch();
-  //
-  // useEffect(() => {
-  //   dispatch(getPetInfo());
-  // }, [dispatch]);
   const adminModal = (type, value) => {
-    setIsShown(!value);
     setIsModalShow(type);
   };
 
@@ -49,9 +43,9 @@ const PetsData = ({ pets, setPets }) => {
     <PetsWrapper>
       <PetsTitleWrapper>
         <UserPetsTitle>Pets:</UserPetsTitle>
-        <PetBtnWrapper>
+        <PetBtnWrapper onClick={() => adminModal('step1', false)}>
           <AddPetTitleBtn>Add</AddPetTitleBtn>
-          <AddPetBtn onClick={() => adminModal('step1', false)} />
+          <AddPetBtn />
         </PetBtnWrapper>
       </PetsTitleWrapper>
       {isModalShow === 'step1' && (
@@ -70,17 +64,14 @@ const PetsData = ({ pets, setPets }) => {
           initialValuesModalData={initialValuesModalData}
         />
       )}
-      {isPets ? (
+      {userPets.length ? (
         <PetList />
       ) : (
         <NonPetWrapper>
           <p>No pets</p>
         </NonPetWrapper>
       )}
-      {/*{isLoading && !error*/}
-      {/*  ? <Loader />*/}
-      {/*  : <PetsList dataPets={pets} setDataPets={setPets} />*/}
-      {/*}*/}
+      {isLoading && !error ? <Loader /> : <PetsList dataPets={pets} />}
     </PetsWrapper>
   );
 };
--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -3,7 +3,6 @@ import UserData from 'components/UserPage/UserData/UserData';
 import PetsData from 'components/UserPage/PetsData/PetsData';
 import { UserPageWrapper } from './UserPage.styled';
 import { Container } from '../../components/Container/Container.styled';
-import { useUser } from 'hooks/useUser';
 
 const UserPage = () => {
   return (
