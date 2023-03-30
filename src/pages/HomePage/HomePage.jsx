1a98f1ad5dbeac2df78411a81149fce45ba80435
Yevhenns <gifestum@gmail.com>
2023-03-30 11:53:35 +0300

    ment

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import img from '../../staticImages/home.png';
 import heart from '../../staticImages/home-heart.png';
-import { Home, Title, Img, Heart } from './HomePage.styled';
+import { Home, Title, Img, Heart, Container } from './HomePage.styled';
 
 const HomePage = () => {
   return (
     <Home>
-      <Title>Take good care of your small pets</Title>
-      <Img src={img} alt="Labrador" />
-      <Heart src={heart} alt="Heart" />
+      <Container>
+        <Title>Take good care of your small pets</Title>
+        <Img src={img} alt="Labrador" />
+        <Heart src={heart} alt="Heart" />
+      </Container>
     </Home>
   );
 };
