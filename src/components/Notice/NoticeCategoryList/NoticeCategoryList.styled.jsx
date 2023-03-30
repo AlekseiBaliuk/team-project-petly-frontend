9af32bc4c7a39ba7486155a2cdd2b1e64f62c702
Yurkes2020 <yurkes2020@gmail.com>
2023-03-30 11:39:18 +0300

    fix fis

--- src/components/Notice/NoticeCategoryList/NoticeCategoryList.styled.jsx
+++ src/components/Notice/NoticeCategoryList/NoticeCategoryList.styled.jsx
@@ -2,9 +2,15 @@ import styled from 'styled-components';
 import { ReactComponent as Cat } from 'staticImages/catUp.svg';
 import { device } from '../../../devices';
 
-const ImageBc = styled('img')`
-  width: 100%;
-  height: 100%;
+const Noads = styled('p')`
+  text-align: center;
+  color: #d49c3c;
+  font-size: 80px;
+  text-shadow: 0 1px 0 #dcca0d, 0 2px 0 #d2c218, 0 3px 0 #bbb, 0 4px 0 #b9b9b9,
+    0 5px 0 #aaa, 0 6px 1px rgba(0, 0, 0, 0.1), 0 0 5px rgba(0, 0, 0, 0.1),
+    0 1px 3px rgba(0, 0, 0, 0.3), 0 3px 5px rgba(0, 0, 0, 0.2),
+    0 5px 10px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.2),
+    0 20px 20px rgba(0, 0, 0, 0.15);
 `;
 
 const Grid = styled('ul')`
@@ -42,6 +48,6 @@ const Scroll = styled(Cat)`
   }
 `;
 
-const style = { Scroll, Grid, ImageBc };
+const style = { Scroll, Grid, Noads };
 
 export default style;
